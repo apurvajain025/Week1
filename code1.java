c7a2e100c12eb6e4327314ab135ec75dc993138c
apurvajain025 <apurvajain025@gmail.com>
2026-09-12 17:44:06 +0530

    main

---
+++ code1.java
@@ -0,0 +1,44 @@
+public class Q1_DuplicateSeats {
+
+    static void checkDuplicateSeats(int[] seatNumbers) {
+
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+
+                if (seatNumbers[i] == seatNumbers[j]) {
+
+                    // Check whether this duplicate was already printed
+                    boolean alreadyPrinted = false;
+
+                    for (int k = 0; k < i; k++) {
+                        if (seatNumbers[k] == seatNumbers[i]) {
+                            alreadyPrinted = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyPrinted) {
+                        System.out.println(
+                            "Duplicate Seat Number Found: " + seatNumbers[i]
+                        );
+                        duplicateFound = true;
+                    }
+                }
+            }
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] seats = {101, 102, 103, 102, 105};
+
+        checkDuplicateSeats(seats);
+    }
+}
